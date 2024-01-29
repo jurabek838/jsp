eda59b2da1622a9a7a21f5396b19e6d9900f6b1a
jurabek <nematovj708@gmail.com>
2024-01-29 14:22:05 +0500

    jsp

---
+++ assets/style/js/app.js
@@ -0,0 +1,10 @@
+alert("Saytimizga xush kilipsiz ");
+prompt("ismingizdi qoldiring");
+prompt("Familyangizni qoldiring");
+prompt("yoshingizni qoldiring");~
+prompt("Otangizdi ismini qoldiring");
+prompt("Onangizdi ismini qoldiring")
+prompt("Oylada nechi kishi siz")
+prompt("yashash manzilingizdi qoldiring");
+prompt("Telif raqamingizdi qoldiring");
+confirm("tabriklaymiz siz saytimizda o`tingiz tez orada sizga aloqaga chiqamiz ")
