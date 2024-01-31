58ddbdb46723dc0e8a1e32b155305005fb229254
jurabek <nematovj708@gmail.com>
2024-01-31 09:44:11 +0500

    js

--- assets/style/js/app.js
+++ assets/style/js/app.js
@@ -1,10 +1,42 @@
 alert("Saytimizga xush kilipsiz ");
 prompt("ismingizdi qoldiring");
 prompt("Familyangizni qoldiring");
-prompt("yoshingizni qoldiring");~
-prompt("Otangizdi ismini qoldiring");
+prompt("yoshingizni qoldiring"); ~
+    prompt("Otangizdi ismini qoldiring");
 prompt("Onangizdi ismini qoldiring")
 prompt("Oylada nechi kishi siz")
 prompt("yashash manzilingizdi qoldiring");
 prompt("Telif raqamingizdi qoldiring");
 confirm("tabriklaymiz siz saytimizda o`tingiz tez orada sizga aloqaga chiqamiz ")
+
+const numr1 = 1234;
+const numr2 = 1234;
+const numr3 = 1234;
+const numr4 = 1234;
+const numr5 = 1234;
+const numr6 = 1234;
+const numr7 = 1234;
+const numr8 = 1234;
+const numr9 = 1234;
+const numr10 = 1234;
+
+let card1 = 666;
+let card2 = 666;
+let card3 = 666;
+let card4 = 666;
+let card5 = 666;
+let card6 = 666;
+let card7 = 666;
+let card8 = 666;
+let card9= 666;
+let card10 = 666;
+
+var box1 = 61266;
+var box3 = 666;
+var box4 = 664246;
+var box5 = 62466;
+var box6 = 64266;
+var box7 = 6266;
+var box8 = 66226;
+var box9 = 66246;
+var box10 = 626;
